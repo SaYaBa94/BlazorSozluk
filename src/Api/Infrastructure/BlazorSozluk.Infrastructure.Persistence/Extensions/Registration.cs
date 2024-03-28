a05dc7e89cfa55a344152e2ccf57dcb5d39db570
Yasin <sadriyasin.balkan@vericom.com.tr>
2024-03-28 18:41:46 +0300

    Add project files.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Extensions/Registration.cs
@@ -0,0 +1,33 @@
+﻿using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Infrastructure.Persistence.Context;
+using BlazorSozluk.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddInfraStructureRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connStr = configuration["BlazorSozlukDbConnectionString"].ToString();
+            services.AddDbContext<BlazorSozlukContext>(conf =>
+            {
+                conf.UseSqlServer(connStr);
+            });
+
+            //var seedData = new SeedData();
+            //seedData.SeedAsync(configuration).GetAwaiter().GetResult();
+
+            services.AddScoped<IUserRepository, UserReporsitory>();
+
+            return services;
+        }
+    }
+}
